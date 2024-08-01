124e4688e9d2109928f9aa44dcbcd9acf5658cb9
escapist <719695260@qq.com>
2024-08-01 23:10:11 +0800

    feat: 增加redis数据上报功能

--- dynamic-thread-pool-spring-boot-stater/src/main/java/cn/comradexy/middleware/sdk/domain/model/valobj/RegistryEnumVO.java
+++ dynamic-thread-pool-spring-boot-stater/src/main/java/cn/comradexy/middleware/sdk/domain/model/valobj/RegistryEnumVO.java
@@ -12,7 +12,7 @@ import lombok.Getter;
  */
 @AllArgsConstructor
 @Getter
-public enum RedistryEnumVO {
+public enum RegistryEnumVO {
     THREAD_POOL_CONFIG_LIST_KEY("THREAD_POOL_CONFIG_LIST_KEY", "线程池配置列表"),
     THREAD_POOL_CONFIG_PARAMETER_LIST_KEY("THREAD_POOL_CONFIG_PARAMETER_LIST_KEY", "线程池配置参数"),
     DYNAMIC_THREAD_POOL_REDIS_TOPIC("DYNAMIC_THREAD_POOL_REDIS_TOPIC", "动态线程池监听主题配置");
--- dynamic-thread-pool-spring-boot-stater/src/main/java/cn/comradexy/middleware/sdk/registry/redis/RedisRegistry.java
+++ dynamic-thread-pool-spring-boot-stater/src/main/java/cn/comradexy/middleware/sdk/registry/redis/RedisRegistry.java
@@ -1,10 +1,13 @@
 package cn.comradexy.middleware.sdk.registry.redis;
 
 import cn.comradexy.middleware.sdk.domain.model.entity.ThreadPoolConfigEntity;
+import cn.comradexy.middleware.sdk.domain.model.valobj.RegistryEnumVO;
 import cn.comradexy.middleware.sdk.registry.IRegistry;
+import org.redisson.api.RBucket;
+import org.redisson.api.RList;
 import org.redisson.api.RedissonClient;
 
-import javax.annotation.Resource;
+import java.time.Duration;
 import java.util.List;
 
 /**
@@ -24,11 +27,20 @@ public class RedisRegistry implements IRegistry {
     @Override
     public void reportThreadPool(List<ThreadPoolConfigEntity> threadPoolEntities) {
         // TODO: 上报所有线程池信息
+        RList<ThreadPoolConfigEntity> list =
+                redissonClient.getList(RegistryEnumVO.THREAD_POOL_CONFIG_LIST_KEY.getKey());
+        list.delete();
+        list.addAll(threadPoolEntities);
     }
 
     @Override
     public void reportThreadPoolConfigParameter(ThreadPoolConfigEntity threadPoolConfigEntity) {
         // TODO: 上报单个线程池的配置参数
+        String cacheKey = RegistryEnumVO.THREAD_POOL_CONFIG_PARAMETER_LIST_KEY.getKey() +
+                "_" + threadPoolConfigEntity.getAppName() +
+                "_" + threadPoolConfigEntity.getThreadPoolName();
+        RBucket<ThreadPoolConfigEntity> bucket = redissonClient.getBucket(cacheKey);
+        bucket.set(threadPoolConfigEntity, Duration.ofDays(30));
     }
 
 }
