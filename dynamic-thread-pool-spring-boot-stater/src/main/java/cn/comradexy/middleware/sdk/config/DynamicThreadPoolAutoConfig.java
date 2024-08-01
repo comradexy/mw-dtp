b7c0823af3023ec6baac602759557f6bc633fae4
escapist <719695260@qq.com>
2024-08-01 23:20:54 +0800

    feat: 增加redisson配置

--- dynamic-thread-pool-spring-boot-stater/src/main/java/cn/comradexy/middleware/sdk/config/DynamicThreadPoolAutoConfig.java
+++ dynamic-thread-pool-spring-boot-stater/src/main/java/cn/comradexy/middleware/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -8,10 +8,12 @@ import cn.comradexy.middleware.sdk.trigger.job.ThreadPoolDataReportJob;
 import org.apache.commons.lang.StringUtils;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,16 +32,33 @@ import java.util.concurrent.ThreadPoolExecutor;
  */
 @Configuration
 @EnableScheduling
+@EnableConfigurationProperties(DynamicThreadPoolAutoProperties.class)
 public class DynamicThreadPoolAutoConfig {
     private final Logger logger = LoggerFactory.getLogger(DynamicThreadPoolAutoConfig.class);
 
     @Bean("redissonClient")
-    @ConditionalOnMissingBean(RedissonClient.class) // 未加载RedissonClient时，才会加载
     public RedissonClient redissonClient(DynamicThreadPoolAutoProperties properties) {
         Config config = new Config();
-        // TODO: 配置 redisson
+        // 根据需要可以设定编解码器；https://github.com/redisson/redisson/wiki/4.-%E6%95%B0%E6%8D%AE%E5%BA%8F%E5%88%97%E5%8C%96
+        config.setCodec(JsonJacksonCodec.INSTANCE);
+
+        config.useSingleServer()
+                .setAddress("redis://" + properties.getHost() + ":" + properties.getPort())
+                .setPassword(properties.getPassword())
+                .setConnectionPoolSize(properties.getPoolSize())
+                .setConnectionMinimumIdleSize(properties.getMinIdleSize())
+                .setIdleConnectionTimeout(properties.getIdleTimeout())
+                .setConnectTimeout(properties.getConnectTimeout())
+                .setRetryAttempts(properties.getRetryAttempts())
+                .setRetryInterval(properties.getRetryInterval())
+                .setPingConnectionInterval(properties.getPingInterval())
+                .setKeepAlive(properties.isKeepAlive())
+        ;
 
         RedissonClient redissonClient = Redisson.create(config);
+
+        logger.info("动态线程池，注册器（redis）链接初始化完成。{} {} {}", properties.getHost(), properties.getPoolSize(),
+                !redissonClient.isShutdown());
 
         return redissonClient;
     }
@@ -50,7 +69,8 @@ public class DynamicThreadPoolAutoConfig {
     }
 
     @Bean
-    public ThreadPoolDataReportJob threadPoolDataReportJob(IDynamicThreadPoolService dynamicThreadPoolService, IRegistry registry) {
+    public ThreadPoolDataReportJob threadPoolDataReportJob(IDynamicThreadPoolService dynamicThreadPoolService,
+                                                           IRegistry registry) {
         return new ThreadPoolDataReportJob(dynamicThreadPoolService, registry);
     }
 
@@ -63,6 +83,9 @@ public class DynamicThreadPoolAutoConfig {
             appName = "缺省";
             logger.warn("应用未配置 spring.application.name, 无法获取到应用名称！");
         }
+
+        // TODO: 获取缓存数据，设置本地线程池配置
+
 
         return new DynamicThreadPoolService(appName, threadPoolExecutorMap);
     }
