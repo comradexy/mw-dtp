60d4050d48834ae7c132b7feca062d3652985fb1
escapist <719695260@qq.com>
2024-08-02 19:20:24 +0800

    feat: 增加启动时缓存数据上报

--- dynamic-thread-pool-spring-boot-stater/src/main/java/cn/comradexy/middleware/sdk/config/DynamicThreadPoolAutoConfig.java
+++ dynamic-thread-pool-spring-boot-stater/src/main/java/cn/comradexy/middleware/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -2,6 +2,7 @@ package cn.comradexy.middleware.sdk.config;
 
 import cn.comradexy.middleware.sdk.domain.DynamicThreadPoolService;
 import cn.comradexy.middleware.sdk.domain.IDynamicThreadPoolService;
+import cn.comradexy.middleware.sdk.domain.model.entity.ThreadPoolConfigEntity;
 import cn.comradexy.middleware.sdk.registry.IRegistry;
 import cn.comradexy.middleware.sdk.registry.redis.RedisRegistry;
 import cn.comradexy.middleware.sdk.trigger.job.ThreadPoolDataReportJob;
@@ -12,15 +13,16 @@ import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
@@ -76,16 +78,40 @@ public class DynamicThreadPoolAutoConfig {
 
     @Bean("dynamicThreadPoolService")
     public DynamicThreadPoolService dynamicThreadPoolService(ApplicationContext applicationContext,
-                                                             Map<String, ThreadPoolExecutor> threadPoolExecutorMap) {
+                                                             Map<String, ThreadPoolExecutor> threadPoolExecutorMap,
+                                                             IRegistry registry) {
         String appName = applicationContext.getEnvironment().getProperty("spring.application.name");
+
         // 如果没有配置应用名，则使用缺省名，同时打印警告日志
         if (StringUtils.isBlank(appName)) {
             appName = "缺省";
             logger.warn("应用未配置 spring.application.name, 无法获取到应用名称！");
         }
 
-        // TODO: 获取缓存数据，设置本地线程池配置
-
+        // 获取缓存数据，设置本地线程池配置
+        Set<String> threadPoolNames = threadPoolExecutorMap.keySet();
+        List<ThreadPoolConfigEntity> threadPoolConfigEntities = new ArrayList<>();
+        for (String threadPoolName : threadPoolNames) {
+            ThreadPoolExecutor threadPoolExecutor = threadPoolExecutorMap.get(threadPoolName);
+            if (null == threadPoolExecutor) {
+                logger.error("线程池为null，线程池名称：{}", threadPoolName);
+                continue;
+            }
+            ThreadPoolConfigEntity threadPoolConfigEntity = ThreadPoolConfigEntity.builder()
+                    .appName(appName)
+                    .threadPoolName(threadPoolName)
+                    .corePoolSize(threadPoolExecutor.getCorePoolSize())
+                    .maximumPoolSize(threadPoolExecutor.getMaximumPoolSize())
+                    .activeCount(threadPoolExecutor.getActiveCount())
+                    .poolSize(threadPoolExecutor.getPoolSize())
+                    .queueType(threadPoolExecutor.getQueue().getClass().getSimpleName())
+                    .queueSize(threadPoolExecutor.getQueue().size())
+                    .remainingCapacity(threadPoolExecutor.getQueue().remainingCapacity())
+                    .build();
+            threadPoolConfigEntities.add(threadPoolConfigEntity);
+            registry.reportThreadPoolConfigParameter(threadPoolConfigEntity);
+        }
+        registry.reportThreadPool(threadPoolConfigEntities);
 
         return new DynamicThreadPoolService(appName, threadPoolExecutorMap);
     }
