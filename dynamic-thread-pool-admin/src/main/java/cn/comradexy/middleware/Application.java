1542f09fe7853fd508a48be79117a4d9f82dd64a
dxy <daixianyou@zjut.edu.cn>
2024-08-06 21:02:30 +0800

    feat: 增加admin管理端

---
+++ dynamic-thread-pool-admin/src/main/java/cn/comradexy/middleware/Application.java
@@ -0,0 +1,20 @@
+package cn.comradexy.middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * admin启动类
+ *
+ * @Author: ComradeXY
+ * @CreateTime: 2024-08-06
+ * @Description: admin启动类
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
